e730b6740a908916a86049111b58776d7bb19ce0
JacopoLombardi <lombajacopo@gmail.com>
2024-06-14 15:50:39 +0200

    [add] scaffolding view dashboard

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,18 @@
+
+@extends('layouts.guest')
+
+
+@section('content')
+
+    <div class="mt-5">
+        <h1 class="text-center">Home sito pubblico</h1>
+    </div>
+
+    
+    <div class="d-flex container mt-5">
+
+
+
+    </div>
+
+@endsection
