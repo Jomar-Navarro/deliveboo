ad67de73ebf385ee9260613bfd36a92d33ae51dc
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-06-14 14:55:56 +0200

    [add] migration & model di Restaurant

---
+++ app/Models/Restaurant.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'address',
+        'vat_number',
+        'image',
+    ];
+}
---
+++ database/migrations/2024_06_14_124838_create_restaurants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 75);
+            $table->string('slug', 150)->unique();
+            $table->text('description')->nullable();
+            $table->string('address', 250);
+            $table->string('vat_number', 11);
+            $table->text('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
