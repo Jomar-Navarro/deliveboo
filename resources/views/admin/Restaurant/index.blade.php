21a62cc502576d48c59730a7df3b9df5b3a5f49b
simoneaucello <simoneaucello@gmail.com>
2024-06-16 22:43:55 +0200

    [add] rest img

--- resources/views/admin/Restaurant/index.blade.php
+++ resources/views/admin/Restaurant/index.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.admin')
 
 @section('content')
-  <div class="container text-center mt-5">
-    <h1>My Restaurant</h1>
-    <div class="row">
-      @if (session('success'))
-        <div class="alert alert-success">
-          {{ session('success') }}
+    <div class="container text-center mt-5">
+        <h1>My Restaurant</h1>
+        <div class="row">
+            @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
+            @foreach ($restaurant as $item)
+                <div class="col-4">
+                    <h1>{{ $item->name }}</h1>
+                    <img class="w-100 rest-img" src="{{ $item->image }}" alt="">
+                </div>
+            @endforeach
         </div>
-      @endif
-      @foreach ($restaurant as $item)
-        <div class="col-4">
-          <h1>{{ $item->name }}</h1>
-          <img class="w-100" src="{{ $item->image }}" alt="">
-        </div>
-      @endforeach
     </div>
-  </div>
 @endsection
