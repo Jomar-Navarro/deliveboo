bb6236dd3a7600af391db238398cf50f71b4c1e0
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-06-17 21:44:33 +0200

    [add] style

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,51 +1,66 @@
 <aside class="text-white">
   <nav class="d-flex flex-column justify-content-between h-100">
     <ul class="d-flex flex-column align-items-start mt-3">
-      <div>
-        <li>
-          <a href="{{ route('admin.home') }}">
-            <i class="fa-solid fa-house"></i>
-            <span>Home</span>
-          </a>
-        </li>
+      <div class="">
 
-        <li>
-          <a href="{{ route('admin.restaurant.index') }}">
-            <i class="fa-solid fa-utensils"></i>
-            <span>I Miei Ristoranti</span>
-          </a>
-        </li>
+        <div class="py-3">
+            <h6 class="text-uppercase">Dashboard</h6>
+            <li class="">
+              <a href="{{ route('admin.home') }}">
+                <i class="fa-solid fa-house"></i>
+                <span>Home</span>
+              </a>
+            </li>
+        </div>
 
-        <li>
-          <a href={{ route('admin.restaurant.create') }}>
-            <i class="fa-solid fa-plus"></i>
-            <span>Aggiungi Ristoranti</span>
-          </a>
-        </li>
-        <li>
-          <a href="{{ route('admin.dish.index') }}">
-            <i class="fa-solid fa-bowl-rice"></i>
-            <span>Piatti</span>
-          </a>
-        </li>
-        <li>
-          <a href={{ route('admin.dish.create') }}>
-            <i class="fa-solid fa-plus"></i>
-            <span>Aggiungi Piatto</span>
-          </a>
-        </li>
+        <div class="border-top border-bottom py-3">
+            <h6 class="text-uppercase mt-2">Ristoranti</h6>
+            <li>
+              <a href="{{ route('admin.restaurant.index') }}">
+                <i class="fa-solid fa-utensils"></i>
+                <span>I Miei Ristoranti</span>
+              </a>
+            </li>
 
-        <li>
-          <a href={{ route('admin.dish.trashed') }}>
-            <i class="fa-solid fa-trash-arrow-up"></i>
-            <span>Piatti Eliminati</span>
-          </a>
-        </li>
+            <li>
+              <a href={{ route('admin.restaurant.create') }}>
+                <i class="fa-solid fa-plus"></i>
+                <span>Aggiungi Ristoranti</span>
+              </a>
+            </li>
+        </div>
+
+        <div class="border-bottom py-3">
+            <h6 class="text-uppercase mt-2">Piatti</h6>
+            <li>
+              <a href="{{ route('admin.dish.index') }}">
+                <i class="fa-solid fa-bowl-rice"></i>
+                <span>Piatti</span>
+              </a>
+            </li>
+
+            <li>
+              <a href={{ route('admin.dish.create') }}>
+                <i class="fa-solid fa-plus"></i>
+                <span>Aggiungi Piatto</span>
+              </a>
+            </li>
+        </div>
+
+        <div class="py-3">
+            <h6></h6>
+            <li>
+              <a href={{ route('admin.dish.trashed') }}>
+                <i class="fa-solid fa-trash-arrow-up"></i>
+                <span>Piatti Eliminati</span>
+              </a>
+            </li>
+        </div>
 
       </div>
-
     </ul>
-    <div class="mb-3">
+
+    <div class=" mb-3">
       <ul>
         <li>
           <a href="#">
@@ -55,5 +70,6 @@
         </li>
       </ul>
     </div>
+
   </nav>
 </aside>
--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,24 +1,59 @@
 
 <header>
-    <nav class="navbar navbar-expand-lg fs-5 fw-semibold text-white">
-        <div class="container-fluid mx-5">
-            <a class="navbar-brand ms-5 px-4 text-white" href="">Visualizza il Sito</a>
+    <nav class="navbar navbar-expand-lg text-white">
+        <div class="container-fluid h-100">
 
-            <div class="d-flex align-items-center">
-                <div class="me-5">
-                    <form class="d-flex" action="" method="GET" role="search">
-                        <input class="form-control me-3" type="search" placeholder="Search" name="stringSearch">
-                        <button class="btn btn-success me-5" type="submit">Cerca</button>
-                    </form>
+            <div class="d-flex h-100 align-items-center w-75">
+
+                <div class="logo d-flex justify-content-center">
+                    <img class="rounded-circle img-fluid" src="/img/logo-dark.png" alt="">
+                    {{-- <h3 class="d-flex align-items-center fw-bolder ps-2">Deliboo</h3> --}}
                 </div>
 
-                <p class="fw-bold  text-capitalize fs-4 m-0 me-5">{{ Auth::user()->name }}</p>
-
-                <form class="mb-0" action="{{ route('logout') }}" method="POST">
-                    @csrf
-                    <button type="submit" class="btn btn-danger fw-semibold fs-5 rounded-5 px-3 py-1">Log Out</button>
-                </form>
+                <div class="searchbar w-50">
+                    <form class="d-flex" action="" method="GET" role="search">
+                        <div class="search d-flex align-items-center">
+                            <span class="pe-2">
+                                <i class="fa-solid fa-magnifying-glass text-secondary"></i>
+                            </span>
+                            <input class="form-control-plaintext me-3" type="search" placeholder="Search" name="stringSearch">
+                        </div>
+                    </form>
+                </div>
             </div>
+
+            <div class="d-flex align-items-center ">
+
+                <div class="d-flex align-items-center justify-content-between">
+                    <div class="mx-2 fs-5"><i class="fa-regular fa-bell"></i></div>
+                    <div class="mx-2 fs-5"><i class="ri-apps-2-line font-22"></i></div>
+                </div>
+
+                <div class="d-flex align-items-center p-3">
+                    <div class="logo-user d-flex justify-content-center">
+                        <img class="rounded-circle img-fluid" src="/img/logo-dark.png" alt="">
+                    </div>
+                    <li class="nav-item dropdown list-group px-2">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right bg-body-secondary" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ url('admin') }}">{{__('Dashboard')}}</a>
+                            <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                            document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                </div>
+            </div>
+
         </div>
     </nav>
 </header>
