871070968d1567e928814cc8401294481b25d2e7
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-06-18 15:12:03 +0200

    [fix] login & register

--- resources/views/admin/Dish/index.blade.php
+++ resources/views/admin/Dish/index.blade.php
@@ -14,22 +14,20 @@
       <table class="table m-0">
         <thead>
           <tr>
-            <th scope="col">id</th>
             <th scope="col">Nome</th>
             <th scope="col">Prezzo</th>
             <th scope="col">Descrizione</th>
             <th scope="col">Visibilità</th>
-            <th scope="col">Funzioni</th>
+            <th scope="col">Azioni</th>
           </tr>
         </thead>
         <tbody>
           @foreach ($dishes as $item)
             <tr>
-              <th scope="row">{{ $item->id }}</th>
               <td>{{ $item->dish_name }}</td>
               <td>&euro; {{ $item->price }}</td>
               <td>{{ $item->description }}</td>
-              <td>{{ $item->is_visible }}</td>
+              <td>{{ $item->is_visible ? 'Sì' : 'No' }}</td>
               <td>
                 <div class="d-flex">
 
