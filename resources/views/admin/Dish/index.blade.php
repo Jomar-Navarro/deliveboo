add4bd5a847444f9629d38f950047cb21343e3e2
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-06-18 15:40:48 +0200

    [fix] merge conflict

--- resources/views/admin/Dish/index.blade.php
+++ resources/views/admin/Dish/index.blade.php
@@ -11,6 +11,26 @@
 
         <div>
 
+        <table class="table m-0">
+        <thead>
+          <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Prezzo</th>
+            <th scope="col">Descrizione</th>
+            <th scope="col">Visibilità</th>
+            <th scope="col">Azioni</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($dishes as $item)
+            <tr>
+              <td>{{ $item->dish_name }}</td>
+              <td>{{ $item->price }}</td>
+              <td>{{ $item->description }}</td>
+              <td>{{ $item->is_visible ? 'Sì' : 'No' }}</td>
+              <td>
+
+                                <div class="d-flex">
 
                                     <a href="{{ route('admin.dish.show', $item) }}" class="btn btn-outline-success m-2">
                                         <i class="fa-solid fa-eye "></i></a>
