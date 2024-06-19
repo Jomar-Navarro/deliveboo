d9b410519e29c5af032ba7535704ac0be9483aa6
simoneaucello <simoneaucello@gmail.com>
2024-06-19 10:48:20 +0200

    [add] types restaurants relations

--- database/seeders/RestaurantTypeTableSeeder.php
+++ database/seeders/RestaurantTypeTableSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Restaurant;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Type;
+use Illuminate\Support\Facades\DB;
 
 class RestaurantTypeTableSeeder extends Seeder
 {
@@ -14,19 +15,70 @@ class RestaurantTypeTableSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 0; $i < 50; $i++) {
+        // for ($i = 0; $i < 50; $i++) {
 
-            $restaurant = Restaurant::inRandomOrder()->first();
-
-
-            $type_id = Type::inRandomOrder()->first()->id;
+        //     $restaurant = Restaurant::inRandomOrder()->first();
 
 
-            $restaurant->types()->attach($type_id);
+        //     $type_id = Type::inRandomOrder()->first()->id;
 
-            // if (!$restaurant->types()->where('type_id', $type_id)->exists()) {
-            //     $restaurant->types()->attach($type_id);
-            // }
-        }
+
+        //     $restaurant->types()->attach($type_id);
+
+        $restaurantTypes = [
+            ['restaurant_id' => 1, 'type_id' => 1],
+            ['restaurant_id' => 1, 'type_id' => 9],
+            ['restaurant_id' => 1, 'type_id' => 8],
+            ['restaurant_id' => 1, 'type_id' => 7],
+            ['restaurant_id' => 2, 'type_id' => 1],
+            ['restaurant_id' => 2, 'type_id' => 8],
+            ['restaurant_id' => 3, 'type_id' => 2],
+            ['restaurant_id' => 3, 'type_id' => 1],
+            ['restaurant_id' => 3, 'type_id' => 5],
+            ['restaurant_id' => 3, 'type_id' => 6],
+            ['restaurant_id' => 3, 'type_id' => 7],
+            ['restaurant_id' => 3, 'type_id' => 9],
+            ['restaurant_id' => 3, 'type_id' => 3],
+            ['restaurant_id' => 4, 'type_id' => 2],
+            ['restaurant_id' => 4, 'type_id' => 3],
+            ['restaurant_id' => 4, 'type_id' => 4],
+            ['restaurant_id' => 4, 'type_id' => 5],
+            ['restaurant_id' => 4, 'type_id' => 6],
+            ['restaurant_id' => 4, 'type_id' => 9],
+            ['restaurant_id' => 5, 'type_id' => 3],
+            ['restaurant_id' => 5, 'type_id' => 2],
+            ['restaurant_id' => 5, 'type_id' => 8],
+            ['restaurant_id' => 5, 'type_id' => 7],
+            ['restaurant_id' => 6, 'type_id' => 3],
+            ['restaurant_id' => 6, 'type_id' => 7],
+            ['restaurant_id' => 6, 'type_id' => 8],
+            ['restaurant_id' => 7, 'type_id' => 4],
+            ['restaurant_id' => 7, 'type_id' => 7],
+            ['restaurant_id' => 7, 'type_id' => 8],
+            ['restaurant_id' => 8, 'type_id' => 4],
+            ['restaurant_id' => 8, 'type_id' => 7],
+            ['restaurant_id' => 9, 'type_id' => 5],
+            ['restaurant_id' => 9, 'type_id' => 8],
+            ['restaurant_id' => 10, 'type_id' => 5],
+            ['restaurant_id' => 10, 'type_id' => 8],
+            ['restaurant_id' => 11, 'type_id' => 6],
+            ['restaurant_id' => 11, 'type_id' => 8],
+            ['restaurant_id' => 12, 'type_id' => 6],
+            ['restaurant_id' => 12, 'type_id' => 8],
+            ['restaurant_id' => 13, 'type_id' => 7],
+            ['restaurant_id' => 13, 'type_id' => 1],
+            ['restaurant_id' => 14, 'type_id' => 7],
+            ['restaurant_id' => 14, 'type_id' => 1],
+            ['restaurant_id' => 15, 'type_id' => 8],
+            ['restaurant_id' => 15, 'type_id' => 1],
+            ['restaurant_id' => 16, 'type_id' => 8],
+            ['restaurant_id' => 16, 'type_id' => 1],
+            ['restaurant_id' => 17, 'type_id' => 9],
+            ['restaurant_id' => 17, 'type_id' => 1],
+            ['restaurant_id' => 18, 'type_id' => 9],
+            ['restaurant_id' => 18, 'type_id' => 1],
+        ];
+
+        DB::table('restaurant_type')->insert($restaurantTypes);
     }
 }
