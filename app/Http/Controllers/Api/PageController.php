f94d7bf33489f9504000bcb488db91670c5ee0e8
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-06-20 01:39:48 +0200

    [add] filter restaurant

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -10,14 +10,32 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $restaurants = Restaurant::with('types', 'dishes')->get();
         return response()->json($restaurants);
     }
 
-        public function type(){
+    public function type()
+    {
         $types = Type::all();
         return response()->json($types);
     }
+    public function getFilteredRestaurants(Request $request)
+    {
+        $types = $request->query('types');
 
+        $typesArray = $types ? explode(',', $types) : [];
+
+        if (count($typesArray) == 0) {
+            $restaurants = Restaurant::with('types', 'dishes')->get();
+            return response()->json($restaurants);
+        }
+
+        $restaurants = Restaurant::whereHas('types', function ($query) use ($typesArray) {
+            $query->whereIn('type_name', $typesArray);
+        }, '=', count($typesArray))->get();
+
+        return response()->json($restaurants);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/restaurants', [PageController::class, 'index']);
 Route::get('/types', [PageController::class, 'type']);
+Route::get('/restaurants/filter', [PageController::class, 'getFilteredRestaurants']);
