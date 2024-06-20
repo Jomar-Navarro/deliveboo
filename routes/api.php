8ba9f06fe5b3dd76d8c066ba510b19f98c5ef90d
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-06-20 10:56:40 +0200

    [add] search-bar

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/restaurants', [PageController::class, 'index']);
 Route::get('/types', [PageController::class, 'type']);
 Route::get('/restaurants/filter', [PageController::class, 'getFilteredRestaurants']);
+Route::get('restaurants/search', [PageController::class, 'search']);
