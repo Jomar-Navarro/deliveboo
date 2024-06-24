3be9db9493d16dfd346df2bcfb750ed39d04bc19
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-06-24 16:41:57 +0200

    [add] payment control

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Braintree\Gateway;
+
+class PaymentController extends Controller
+{
+    protected $gateway;
+
+    public function __construct()
+    {
+        $this->gateway = new Gateway([
+            'environment' => 'sandbox',
+            'merchantId' => env('BRAINTREE_MERCHANT_ID'),
+            'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
+            'privateKey' => env('BRAINTREE_PRIVATE_KEY'),
+        ]);
+    }
+
+    public function getClientToken()
+    {
+        $clientToken = $this->gateway->clientToken()->generate();
+        return response()->json(['clientToken' => $clientToken]);
+    }
+
+    public function processPayment(Request $request)
+    {
+        $nonce = $request->nonce;
+        $amount = $request->amount; // Assicurati di inviare l'importo dal frontend
+
+        $result = $this->gateway->transaction()->sale([
+            'amount' => $amount,
+            'paymentMethodNonce' => $nonce,
+            'options' => [
+                'submitForSettlement' => True
+            ]
+        ]);
+
+        if ($result->success) {
+            return response()->json(['success' => true]);
+        } else {
+            return response()->json(['success' => false, 'message' => $result->message]);
+        }
+    }
+}
